ce6c63cfde0ec1acf993b71e5efc9e3bb62e4c2f
Mohib Arsala <mohib.0008@gmail.com>
2020-08-09 15:55:58 -0400

    update from pc win

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt-nodejs");
 const app = express();
+const knex = require("knex")
+
+knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      user : 'your_database_user',
+      password : 'your_database_password',
+      database : 'myapp_test'
+    }
+  });
 
 
 app.use(bodyParser.json());
